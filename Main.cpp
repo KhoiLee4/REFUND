9ce3aee5bcbfcfc59923c1dd01122a472ad4ed7b
KhoiLee <khoile139004@gmail.com>
2023-10-10 21:05:35 +0700

    chuan bi da hoan tat

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
+//Setup
 #include <SFML/Graphics.hpp>
 
 int main()
 {
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
